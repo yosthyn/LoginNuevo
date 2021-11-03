637eaa114aee28e3a59e9a4c7f3f99ab88dcb709
Lxssasd <Lxss.nanjari@gmail.com>
2021-11-03 12:30:16 -0300

    Nuevologin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './public/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+(path: '', component: LoginComponent, pathMatch: 'Full'),
+(path: 'Login', component: '', pathMatch: '')
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { PublicModule } from './public/public.module';
+import { PrivateModule } from './private/private.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicModule,
+    PrivateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
